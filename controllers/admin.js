9f3d870cacee28c438b3f7917b5b3e013c515183
Kyriakos Karagkiozidis <kirk.karagiozidis@gmail.com>
2019-06-27 14:50:17 +0300

    End of lecture 111. End of Section 8.

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,7 @@
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render("add-product", {
+  res.render("admin/add-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
     formsCSS: true,
@@ -11,20 +11,21 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const products = new Product(req.body.title);
+  const title = req.body.title;
+  const imageURL = req.body.imageURL;
+  const price = req.body.price;
+  const description = req.body.description;
+  const products = new Product(title, imageURL, price, description);
   products.save();
   res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
-    res.render("shop", {
+    res.render("admin/products", {
       prods: products,
-      pageTitle: "Shop",
-      path: "/",
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
+      pageTitle: "Admin Products",
+      path: "/admin/products"
     });
   });
 };
