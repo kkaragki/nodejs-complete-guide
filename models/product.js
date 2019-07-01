76cd38e8059eadaecc603ef03039cbccb85958ae
Kyriakos Karagkiozidis <kirk.karagiozidis@gmail.com>
2019-07-01 17:01:44 +0300

    End of lecture 126.

--- models/product.js
+++ models/product.js
@@ -3,6 +3,8 @@ const path = require("path");
 
 const p = path.join(__dirname, "../data", "products.json");
 
+const Cart = require('./cart');
+
 const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
@@ -38,6 +40,18 @@ module.exports = class Product {
         });
       }
     });
+  }
+
+  static deleteById(id) {
+    getProductsFromFile(products => {
+      const product = products.find(prod => prod.id === id);
+      const updatedProducts = products.filter(prod => prod.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+        if(!err) {
+          Cart.deleteProduct(id, product.price);
+        }
+      });
+    })
   }
 
   static fetchAll(cb) {
