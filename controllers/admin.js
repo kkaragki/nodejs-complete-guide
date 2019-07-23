3e5cac57064bf113b3798a07fb70ada1e1d3a82b
Kyriakos Karagkiozidis <kirk.karagiozidis@gmail.com>
2019-07-23 14:07:02 +0300

    End of lecture 266. End of Section 15.

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,8 +4,7 @@ exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    editing: false,
-    isAuthenticated: req.session.isLoggedIn
+    editing: false
   });
 };
 
@@ -47,8 +46,7 @@ exports.getEditProduct = (req, res, next) => {
         pageTitle: 'Edit Product',
         path: '/admin/edit-product',
         editing: editMode,
-        product: product,
-        isAuthenticated: req.session.isLoggedIn
+        product: product
       });
     })
     .catch(err => console.log(err));
@@ -81,12 +79,11 @@ exports.getProducts = (req, res, next) => {
     //   .select('title price -_id')
     //   .populate('userId', 'name')
     .then(products => {
-      console.log(products);
+      // console.log(products);
       res.render('admin/products', {
         prods: products,
         pageTitle: 'Admin Products',
-        path: '/admin/products',
-        isAuthenticated: req.session.isLoggedIn
+        path: '/admin/products'
       });
     })
     .catch(err => {
