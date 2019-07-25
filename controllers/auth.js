4f66f3f6bfb86564f85e77dd0991d0558bfba8f8
Kyriakos Karagkiozidis <kirk.karagiozidis@gmail.com>
2019-07-25 09:59:56 +0300

    Minor changes.

--- controllers/auth.js
+++ controllers/auth.js
@@ -36,7 +36,7 @@ exports.getSignup = (req, res, next) => {
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  
+
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(422).render('auth/login', {
@@ -47,10 +47,10 @@ exports.postLogin = (req, res, next) => {
   }
   User.findOne({ email: email })
     .then(user => {
-      //     if (!user) {
-      //       req.flash('error', 'Invalid email or password.');
-      //       return res.redirect('/login');
-      //     }
+      if (!user) {
+        req.flash('error', 'Invalid email or password.');
+        return res.redirect('/login');
+      }
       bcrypt.compare(password, user.password).then(doMatch => {
         if (doMatch) {
           req.session.isLoggedIn = true;
